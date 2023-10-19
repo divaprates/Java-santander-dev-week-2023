df5c4e75a4a92ec4f72d498dfede53518a5abf77
divaprates <divasilvaprates@gmail.com>
2023-10-19 13:10:18 -0300

    Initial commit

---
+++ src/test/java/com/div/JavaSantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.div;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
