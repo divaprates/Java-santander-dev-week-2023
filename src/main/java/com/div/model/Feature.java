e7fe04d7675784eee655c083bf4a74a386bea18d
divaprates <divasilvaprates@gmail.com>
2023-10-19 19:13:54 -0300

    feat: Mapeamento objeto relacional (ORM)

---
+++ src/main/java/com/div/model/Feature.java
@@ -0,0 +1,9 @@
+package com.div.model;
+
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/div/model/News.java
@@ -0,0 +1,8 @@
+package com.div.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
